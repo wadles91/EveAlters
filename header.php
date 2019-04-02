7858176aa6a0576b37dfe39fa11b4f89c4309736
wadles91 <44385147+wadles91@users.noreply.github.com>
2019-04-02 13:05:48 -0600

    fixing indentation

--- header.php
+++ header.php
@@ -6,10 +6,10 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eve Alters</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eve Alters</title>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
   <header>
